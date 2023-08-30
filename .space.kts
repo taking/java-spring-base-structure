8a112139756b8d93dd44dba6448aeba4aef0492e
taking <taking@duck.com>
2023-08-30 05:14:45 +0000

    Fix: Java Gradle Build Test Script updated

--- .space.kts
+++ .space.kts
@@ -1,11 +1,10 @@
 // https://space.taking.kr/p/main/automation/jobs?repo=java-spring-base-structure&branch=refs%2Fheads%2Fmain&status=actual
 job("Taking Space Automation :: Build and run tests") {
-    container(displayName = "Gradle build", image = "eclipse-temurin:17.0.7_7-jdk") {
-        if (api.gitBranch() == "refs/heads/main"){
-            kotlinScript { api ->
-                // here goes complex logic
-                api.gradlew("build")
-            }
-        }
+  container(displayName = "Gradle build", image = "eclipse-temurin:17.0.7_7-jdk") {
+    kotlinScript { api ->
+      if (api.gitBranch() == "refs/heads/main") {
+        api.gradlew("build")
+      }
     }
+  }
 }
