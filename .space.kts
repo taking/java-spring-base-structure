c136c4ededa8ddba1fab4aba733ccd38555580de
taking <taking@duck.com>
2023-12-06 02:21:31 +0000

    updated

--- .space.kts
+++ .space.kts
@@ -1,22 +1,32 @@
 // https://space.taking.kr/p/main/automation/jobs?repo=java-spring-base-structure&branch=refs%2Fheads%2Fmain&status=actual
 
-job("Qodana") {
-    container("jetbrains/qodana-jvm-community") {
-        env["QODANA_TOKEN"] = Secrets("qodana-token")
-        shellScript {
-            content = """
-               qodana
-               """.trimIndent()
+job("Taking Space Automation") {  
+    // To check a condition, basically, you need a kotlinScript step
+    host(displayName = "Check branch") {
+        kotlinScript { api ->
+            // To pass the result of the condition to other steps, create a job parameter
+            api.parameters["isMainBranch"] = (api.gitBranch() == "refs/heads/main").toString()
         }
     }
-}
 
-job("Taking Space Automation :: Build and run tests") {
-  container(displayName = "Gradle build", image = "eclipse-temurin:17.0.7_7-jdk") {
-    kotlinScript { api ->
-      if (api.gitBranch() == "refs/heads/main") {
-        api.gradlew("build")
+  container(displayName = "qodona Code Inspect", image = "jetbrains/qodana-jvm-community") {
+      env["QODANA_TOKEN"] = Secrets("qodana-token")
+      shellScript {
+          content = """
+              qodana
+              """.trimIndent()
       }
+  }
+
+  container(displayName = "Build Step", image = "eclipse-temurin:17.0.7_7-jdk") {
+    kotlinScript { api ->
+      runIf("{{ isMainBranch }}")
+      // if (api.gitBranch() == "refs/heads/main") {
+        println("Build #" + api.executionNumber())
+        
+        api.gradlew("build")
+        api.space().projects.planning.issues.createIssue()
+      // }
     }
   }
 }
