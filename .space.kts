eb028b9350b143d7e02f62f27537d1e6eeb56b7d
taking <taking@duck.com>
2023-12-06 01:47:20 +0000

    qodona integration

--- .space.kts
+++ .space.kts
@@ -1,4 +1,16 @@
 // https://space.taking.kr/p/main/automation/jobs?repo=java-spring-base-structure&branch=refs%2Fheads%2Fmain&status=actual
+
+job("Qodana") {
+    container("jetbrains/qodana-<linter>") {
+        env["QODANA_TOKEN"] = Secrets("qodana-token")
+        shellScript {
+            content = """
+               qodana
+               """.trimIndent()
+        }
+    }
+}
+
 job("Taking Space Automation :: Build and run tests") {
   container(displayName = "Gradle build", image = "eclipse-temurin:17.0.7_7-jdk") {
     kotlinScript { api ->
